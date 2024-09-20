6728ad8d298859cb30fc4c7cf8ebaebb3c822384
xtanjirox <xtanjirokamado93@gmail.com>
2024-09-20 19:44:20 +0200

    test git

--- financefacile/core/admin.py
+++ financefacile/core/admin.py
@@ -4,3 +4,4 @@ from . import models
 admin.site.register(models.FinanceEntry)
 admin.site.register(models.EntryCategory)
 admin.site.register(models.SeasonEntry)
+admin.site.register(models.SeasonEntry)
